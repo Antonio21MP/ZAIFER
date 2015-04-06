d1b47488641325431dfb2df1f4252369b309394e
José Antonio <antonio_mperdomo@hotmail.com>
2015-04-06 00:49:53 -0600

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,3 +1,7 @@
+//Copyright (C) 2015 Jose Antonio Mejia (Jose34mp).
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
 #ifndef ANIMAL_H
 #define ANIMAL_H
 #include <allegro5/allegro.h>
